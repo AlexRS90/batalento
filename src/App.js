90035223948426db210cae7adaa6ca670238864a
AlexRS90 <aramosbengy@gmail.com>
2023-11-21 17:55:52 -0600

    Try webhook

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sitio en Construcción
+          Sitio en Construcción, esperalo muy pronto.
         </p>
       </header>
     </div>
