d03d1ddf7cdbb913fb31be7b95a6dc7b7270a3aa
AlexRS90 <aramosbengy@gmail.com>
2023-12-01 17:49:36 -0600

    Crate an array with text info

---
+++ src/data.js
@@ -0,0 +1,6 @@
+export const OFFICE_CARDS = [
+  {
+    id: 1,
+    text_info: 'Brindamos servicios personalizados y a la medida de cada una de las necesidades de nuestros clientes. Para lograrlo nos es muy importante mantener una estrecha y continua comunicación tanto con nuestros clientes, como con los candidatos para de esta forma conocer a detalle el objetivo, valores y cultura de cada cliente y en base a esto poder evaluar la experiencia, conocimientos y habilidades de los candidatos, para encontrar el “match” perfecto entre candidato y organización.',
+  },
+];
