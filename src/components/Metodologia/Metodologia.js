dfb4292819c4fca729934abc46bc5c098451cd9d
AlexRS90 <aramosbengy@gmail.com>
2024-01-16 14:24:16 -0600

    Remove Met animation, fix cards height

--- src/components/Metodologia/Metodologia.js
+++ src/components/Metodologia/Metodologia.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { METODOLOGIA } from '../../data';
 
 function Metodologia({ language }) {
-  const [activeClass, setActiveClass] = React.useState(false);
   const {
     title,
     subtitle,
@@ -10,33 +9,16 @@ function Metodologia({ language }) {
     cierre,
   } = METODOLOGIA[0];
   const setId = language === 'ESPAÑOL' ? 'metodology' : 'metodologia';
-  const hiddenDelay = activeClass ? 'active' : undefined; /* eslint-disable-line */
 
-  function reveal() {
-    const reveals = document.querySelectorAll('.reveal-met');
-    for (let i = 0; i < reveals.length; i++) { /* eslint-disable-line */
-      const windowHeight = window.innerHeight;
-      const elementTop = reveals[i].getBoundingClientRect().top;
-      const elementVisible = 500;
-      if (elementTop < windowHeight - elementVisible) {
-        reveals[i].classList.add('active');
-        setActiveClass(true);
-      } else {
-        reveals[i].classList.remove('active');
-        setActiveClass(false);
-      }
-    }
-  }
-  window.addEventListener('scroll', reveal);
   return (
     <section className="met-container parallax-sections" id={setId}>
       <div className="met-title">
         <p className="titulo-secciones">{title}</p>
         <p className="subtitulo-secciones">{subtitle}</p>
       </div>
-      <div className="met-cards reveal-met">
+      <div className="met-cards">
         {cards.map(({ id, title, info }) => (
-          <div className={`met-card-wrapper reveal-card-${id}`} key={Math.random()}>
+          <div className="met-card-wrapper" key={Math.random()}>
             <p className="titulo-texto met-card-num">{id}</p>
             <p className="titulo-texto">{title.toUpperCase()}</p>
             <ul className="met-card-list">
