429d17601b8c969d3dcd3d3e4bb6dfe7db3c7310
AlexRS90 <aramosbengy@gmail.com>
2024-01-19 13:28:40 -0600

    Fix animations

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,26 +5,34 @@ import ArrowsNavigation from '../ArrowsNavigation/ArrowsNavigation';
 
 function Home({ language }) {
   const [card, setCard] = React.useState(0);
-  const [activeClass, setActiveClass] = React.useState(false);/* eslint-disable-line */
-  // const ref = React.useRef(null);
+  const [activeClass, setActiveClass] = React.useState(false);
+  const ref = React.useRef(null);
   const maxCards = OFFICE_CARDS.length - 1;
   const currentCard = OFFICE_CARDS[card];
   const description = language === 'ENGLISH' ? HOME[0] : HOME[1];
-  // const appearOptions = {
-  //   rootMargin: '0px 0px -100px 0px',
-  // };
+  const appearOptions = {
+    rootMargin: '0px 0px -100px 0px',
+  };
 
-  // React.useEffect(() => {
-  //   const observer = new IntersectionObserver(
-  //     ([entry]) => {
-  //       setIsIntersecting(entry.isIntersecting);
-  //     },
-  //     appearOptions,
-  //   );
-  //   console.log(isIntersecting);
-  //   observer.observe(ref.current);
-  //   return () => observer.disconnect();
-  // }, []);
+  React.useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setActiveClass(entry.isIntersecting);
+      },
+      appearOptions,
+    );
+    console.log(activeClass);
+    observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, [activeClass]);
+
+  React.useEffect(() => {
+    if (activeClass) {
+      ref.current.classList.add('appear-on-scroll');
+    } else {
+      ref.current.classList.remove('appear-on-scroll');
+    }
+  }, [activeClass]);
 
   // function reveal() {
   //   const reveals = document.querySelectorAll('.reveal');
@@ -58,7 +66,7 @@ function Home({ language }) {
         </p>
       </div>
       <div className="office">
-        <div className="square-card revelate">
+        <div className="square-card" ref={ref}>
           <ArrowsNavigation card={card} setCard={setCard} maxCards={maxCards} section="home" />
           <CardEstamosListos currentCard={currentCard} card={card} activeClass={activeClass} />
         </div>
